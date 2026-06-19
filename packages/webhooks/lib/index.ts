cd29a25315ce6700d981eebac72fc0a901196f33
kaposke <guilhermebassa@gmail.com>
2026-06-19 14:14:36 -0300

    feat(webhooks): per-connection webhook URL override

--- packages/webhooks/lib/index.ts
+++ packages/webhooks/lib/index.ts
@@ -3,6 +3,7 @@ export { sendAsyncActionWebhook } from './asyncAction.js';
 import { sendSync } from './sync.js';
 
 export { forwardWebhook } from './forward.js';
+export { resolveWebhookSettings } from './utils.js';
 
 type SendSyncParams = Parameters<typeof sendSync>[0];
 
